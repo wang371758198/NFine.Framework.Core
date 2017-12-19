311468eb76c8a8c5ad8f089f53b5a7e74d88412b
wang371758198 <371758198@qq.com>
2017-12-19 20:01:07 +0800

    添加ProcessOn设计前端框架

---
+++ NFine.Web/Areas/SystemManage/Controllers/WorkFlowDesignController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NFine.Web.Areas.SystemManage.Controllers
+{
+    public class WorkFlowDesignController : Controller
+    {
+        /// <summary>
+        /// 工作流设计
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult stop()
+        {
+            return Content("");
+        }
+
+    }
+}
