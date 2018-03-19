d6e712adb92f99d796aa9bd06e84b4cc4bc1b2b5
王俊桥 <371758198@qq.com>
2018-03-19 16:26:12 +0800

    添加自动编译文件，创建docker镜像

--- NFine.Web/build.cmd
+++ NFine.Web/build.cmd
@@ -1,1 +1,17 @@
-锘縟otnet publish -c Release -o publish
+#dotnet publish -c Release -o publish
+echo off
+echo "当前docker中"
+docker images nfine
+
+echo "请输入版本号:"
+set /p version=
+
+dotnet clean
+dotnet publish -c Release -o publish
+
+
+docker build -t nfine:%version% .
+
+echo "创建Repository:nfine  Tag:%version%完成！！！"
+
+
