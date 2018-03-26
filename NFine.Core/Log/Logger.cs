939c00c90ec6bb819d9ec94b6d96a5289795545a
王俊桥 <371758198@qq.com>
2018-03-26 13:48:18 +0800

    修改日志记录

--- NFine.Core/Log/Logger.cs
+++ NFine.Core/Log/Logger.cs
@@ -8,9 +8,11 @@ namespace NFine.Code
     public static class Logger<T>
     {
         private static Logger _logger ;
+        private static Logger _loggerError;
         static Logger()
         {
-             _logger = NLog.LogManager.LoadConfiguration("NLog.config").GetLogger(typeof(T).ToString());
+             _logger = NLog.LogManager.LoadConfiguration("NLog.config").GetLogger("Console."+typeof(T).ToString());
+            _loggerError= NLog.LogManager.LoadConfiguration("NLog.config").GetLogger("Error." + typeof(T).ToString());
         }
 
         public static void Trace(string message)
@@ -34,9 +36,9 @@ namespace NFine.Code
             _logger.Error(message);
         }
 
-        public static void Error(string message, params object[] args) => _logger.Error(message, args);
-        public static void Error(Exception e) => _logger.Error(e,e.Message);
-        public static void Error(string message, Exception e) => _logger.Error(e, message);
+        public static void Error(string message, params object[] args) => _loggerError.Error(message, args);
+        public static void Error(Exception e) => _loggerError.Error(e,e.Message);
+        public static void Error(string message, Exception e) => _loggerError.Error(e, message);
 
     }
 }
