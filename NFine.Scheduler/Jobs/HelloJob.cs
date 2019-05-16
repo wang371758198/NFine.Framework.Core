7b4b0d65bea8a18ef8cd9352320829a375b8a5e5
6mi <371758198@qq.com>
2019-05-16 16:46:10 +0800

    添加NFine.Scheduler调度应用程序

---
+++ NFine.Scheduler/Jobs/HelloJob.cs
@@ -0,0 +1,16 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NFine.Scheduler.Jobs
+{
+    public class HelloJob : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            await Console.Out.WriteLineAsync("Async Hello ！！！"+DateTime.Now.ToString("HH:mm:ss.fff"));
+        }
+    }
+}
